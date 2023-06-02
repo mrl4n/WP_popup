b665662ff33b2e289f5e6d582c650073fe8166a7
mrl4n <59796441+mrl4n@users.noreply.github.com>
2023-06-02 16:36:55 +0100

    Update custom-popup-plugin.php

--- WP popup/custom-popup-plugin.php
+++ WP popup/custom-popup-plugin.php
@@ -8,14 +8,7 @@
  * Author URI: https://github.com/mrl4n
  * License: GPLv2 or later
  * Text Domain: custom-popup-plugin
- * Domain Path: /languages/
  */
-
-// Load plugin text domain for translations
-function custom_popup_load_textdomain() {
-    load_plugin_textdomain('custom-popup-plugin', false, dirname(plugin_basename(__FILE__)) . '/languages/');
-}
-add_action('plugins_loaded', 'custom_popup_load_textdomain');
 
 // Enqueue necessary scripts and styles
 function custom_popup_scripts() {
@@ -39,19 +32,19 @@ function custom_popup_markup() {
     ?>
     <div id="custom-popup" class="custom-popup">
         <div class="custom-popup-content">
-            <img src="<?php echo esc_url(plugins_url('images/logo.png', __FILE__)); ?>" alt="Logo">
-            <h3><?php _e('Welcome to our website!', 'custom-popup-plugin'); ?></h3>
-            <p><?php _e('Enjoy your stay and check out our amazing offers.', 'custom-popup-plugin'); ?></p>
+            <img src="<?php echo plugins_url('images/logo.png', __FILE__); ?>" alt="Logo">
+            <h3><?php echo esc_html(get_option('custom_popup_message', 'Welcome to our website!')); ?></h3>
+            <p><?php echo esc_html(get_option('custom_popup_description', 'Enjoy your stay and check out our amazing offers.')); ?></p>
 
             <!-- Form -->
             <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>">
                 <input type="hidden" name="action" value="custom_popup_submit">
                 <input type="hidden" name="custom_popup_nonce" value="<?php echo esc_attr($nonce); ?>">
-                <input type="text" name="custom_popup_input" placeholder="<?php _e('Enter your input', 'custom-popup-plugin'); ?>">
-                <button type="submit"><?php _e('Submit', 'custom-popup-plugin'); ?></button>
+                <input type="text" name="custom_popup_input" placeholder="<?php echo esc_attr(get_option('custom_popup_placeholder', 'Enter your input')); ?>">
+                <button type="submit"><?php echo esc_html(get_option('custom_popup_button_text', 'Submit')); ?></button>
             </form>
 
-            <button id="custom-popup-close"><?php _e('Close', 'custom-popup-plugin'); ?></button>
+            <button id="custom-popup-close"><?php echo esc_html(get_option('custom_popup_close_text', 'Close')); ?></button>
         </div>
     </div>
     <?php
@@ -60,19 +53,21 @@ add_action('wp_footer', 'custom_popup_markup');
 
 // Initialize the pop-up functionality
 function custom_popup_initialize() {
-    ?>
-    <script type="text/javascript">
-        jQuery(document).ready(function($) {
-            // Show the pop-up
-            $('#custom-popup').fadeIn();
+    if (should_display_custom_popup()) {
+        ?>
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                // Show the pop-up
+                $('#custom-popup').fadeIn();
 
-            // Close the pop-up when the close button is clicked
-            $('#custom-popup-close').on('click', function() {
-                $('#custom-popup').fadeOut();
+                // Close the pop-up when the close button is clicked
+                $('#custom-popup-close').on('click', function() {
+                    $('#custom-popup').fadeOut();
+                });
             });
-        });
-    </script>
-    <?php
+        </script>
+        <?php
+    }
 }
 add_action('wp_footer', 'custom_popup_initialize');
 
@@ -96,49 +91,81 @@ function custom_popup_settings_page() {
 
     // Save settings
     if (isset($_POST['custom_popup_settings_save'])) {
-        $enabled = isset($_POST['custom_popup_enabled']) ? true : false;
-        $message = sanitize_text_field($_POST['custom_popup_message']);
-        $button_text = sanitize_text_field($_POST['custom_popup_button_text']);
-
-        // Save settings to the database
-        update_option('custom_popup_enabled', $enabled);
-        update_option('custom_popup_message', $message);
-        update_option('custom_popup_button_text', $button_text);
+        update_option('custom_popup_enabled', isset($_POST['custom_popup_enabled']) ? true : false);
+        update_option('custom_popup_message', sanitize_text_field($_POST['custom_popup_message']));
+        update_option('custom_popup_description', sanitize_text_field($_POST['custom_popup_description']));
+        update_option('custom_popup_button_text', sanitize_text_field($_POST['custom_popup_button_text']));
+        update_option('custom_popup_placeholder', sanitize_text_field($_POST['custom_popup_placeholder']));
+        update_option('custom_popup_close_text', sanitize_text_field($_POST['custom_popup_close_text']));
+        update_option('custom_popup_selected_pages', isset($_POST['custom_popup_selected_pages']) ? array_map('intval', $_POST['custom_popup_selected_pages']) : array());
 
         // Display a success message
         echo '<div class="notice notice-success"><p>' . __('Settings saved successfully.', 'custom-popup-plugin') . '</p></div>';
     }
 
-    // Get current settings values
+    // Retrieve current settings
     $enabled = get_option('custom_popup_enabled');
     $message = get_option('custom_popup_message');
+    $description = get_option('custom_popup_description');
     $button_text = get_option('custom_popup_button_text');
+    $placeholder = get_option('custom_popup_placeholder');
+    $close_text = get_option('custom_popup_close_text');
+    $selected_pages = get_option('custom_popup_selected_pages', array());
 
-    // Render the settings page HTML
+    // Get all published pages
+    $pages = get_pages();
+
+    // Display the settings page form
     ?>
     <div class="wrap">
-        <h1><?php _e('Custom Pop-up Settings', 'custom-popup-plugin'); ?></h1>
+        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
         <form method="post" action="">
             <table class="form-table">
                 <tr>
-                    <th scope="row"><?php _e('Enable Pop-up', 'custom-popup-plugin'); ?></th>
+                    <th scope="row"><?php _e('Enable Custom Pop-up', 'custom-popup-plugin'); ?></th>
                     <td>
-                        <label for="custom_popup_enabled">
-                            <input type="checkbox" name="custom_popup_enabled" id="custom_popup_enabled" value="1" <?php checked($enabled, 1); ?>>
-                            <?php _e('Enable custom pop-up', 'custom-popup-plugin'); ?>
-                        </label>
+                        <label><input type="checkbox" name="custom_popup_enabled" value="1" <?php checked($enabled, true); ?>><?php _e('Enable the custom pop-up', 'custom-popup-plugin'); ?></label>
                     </td>
                 </tr>
                 <tr>
                     <th scope="row"><?php _e('Pop-up Message', 'custom-popup-plugin'); ?></th>
                     <td>
-                        <textarea name="custom_popup_message" id="custom_popup_message" rows="3"><?php echo esc_textarea($message); ?></textarea>
+                        <input type="text" name="custom_popup_message" class="regular-text" value="<?php echo esc_attr($message); ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"><?php _e('Pop-up Description', 'custom-popup-plugin'); ?></th>
+                    <td>
+                        <textarea name="custom_popup_description" class="regular-text"><?php echo esc_textarea($description); ?></textarea>
                     </td>
                 </tr>
                 <tr>
                     <th scope="row"><?php _e('Button Text', 'custom-popup-plugin'); ?></th>
                     <td>
-                        <input type="text" name="custom_popup_button_text" id="custom_popup_button_text" value="<?php echo esc_attr($button_text); ?>">
+                        <input type="text" name="custom_popup_button_text" class="regular-text" value="<?php echo esc_attr($button_text); ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"><?php _e('Input Placeholder', 'custom-popup-plugin'); ?></th>
+                    <td>
+                        <input type="text" name="custom_popup_placeholder" class="regular-text" value="<?php echo esc_attr($placeholder); ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"><?php _e('Close Button Text', 'custom-popup-plugin'); ?></th>
+                    <td>
+                        <input type="text" name="custom_popup_close_text" class="regular-text" value="<?php echo esc_attr($close_text); ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"><?php _e('Display on Selected Pages', 'custom-popup-plugin'); ?></th>
+                    <td>
+                        <?php foreach ($pages as $page) : ?>
+                            <label>
+                                <input type="checkbox" name="custom_popup_selected_pages[]" value="<?php echo esc_attr($page->ID); ?>" <?php checked(in_array($page->ID, $selected_pages), true); ?>>
+                                <?php echo esc_html($page->post_title); ?>
+                            </label><br>
+                        <?php endforeach; ?>
                     </td>
                 </tr>
             </table>
@@ -148,4 +175,17 @@ function custom_popup_settings_page() {
         </form>
     </div>
     <?php
+}
+
+// Helper function to check if the pop-up should be displayed on the current page
+function should_display_custom_popup() {
+    $enabled = get_option('custom_popup_enabled', false);
+    $selected_pages = get_option('custom_popup_selected_pages', array());
+
+    // Display the pop-up if it is enabled and the current page is in the selected pages list
+    if ($enabled && is_page($selected_pages)) {
+        return true;
+    }
+
+    return false;
 }
